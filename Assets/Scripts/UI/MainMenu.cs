2fd0f04c54644b127f75bd868f775d01fa4a7fa7
DrDoak <luoleizhao2018@u.northwestern.com>
2026-01-23 08:35:49 -0500

    more background elements

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ToggleObjects(GameObject obj)
+    {
+        obj.SetActive(!obj.activeSelf);
+    }
+}
